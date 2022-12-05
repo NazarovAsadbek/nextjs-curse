7c9c67a154ff3e82ed7f73b6c01c9aa27584d515
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-12-05 22:12:38 +0500

    Lesson 2

---
+++ test.ts
@@ -0,0 +1,89 @@
+// let a: number = 4;
+// let b: string = 'Asadbek';
+// let c: boolean = true;
+// let d: string[] = ['12321', '123213', '123213']
+// let e: any = 3;
+//
+// function test(a: string): number | string {
+//     return a
+// }
+//
+// const test2 = (a: number): string | number => {
+//     return a
+// }
+//
+// d = d.map((x: string) => x.toLowerCase());
+//
+// function countCoord(coord: { lat: number, long?: number }) {
+//
+// }
+//
+// function printIt(id: number | string) {
+//     if (typeof id === 'number') {
+//         console.log(a.toFixed(2))
+//     } else if (typeof id === 'string') {
+//         console.log(id.toLowerCase())
+//     }
+// }
+//
+// function getSum(a: number | number[]) {
+//     if (Array.isArray(a)) {
+//         a.forEach(i => i)
+//     }
+// }
+//
+// const voidFunc = (a: string): void => {
+//     console.log(a)
+// }
+//
+// const x: undefined = undefined;
+// const z: null = null;
+
+// type Point = {
+//     x: number,
+//     y: number
+// };
+//
+// type D3Point = Point & {
+//     z: number;
+// }
+//
+// interface iPoint {
+//     x: number,
+//     y: number
+// }
+//
+// interface i3DPoint extends iPoint {
+//     z: number
+// }
+//
+// type stringOrNumber = string | number;
+//
+// function print(coords: stringOrNumber | iPoint | Point) {
+//     console.log(coords)
+// }
+
+// interface iTest {
+//     a: number
+// }
+//
+// interface iTest {
+//     b: number
+// }
+
+// interface IPoint {
+//     x: number,
+//     y: number
+// }
+//
+// interface I3DPoint extends IPoint {
+//     z: number
+// }
+//
+// const c = (point: IPoint) => {
+//     const d: I3DPoint = point as I3DPoint;
+// }
+//
+// const myCanvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+
