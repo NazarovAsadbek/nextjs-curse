3f5736fde5526e7bc567dfb948babb51f8e117ba
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-12-06 21:50:53 +0500

    Chapter 5. Ended

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from 'next/head'
+import React from 'react'
 
 function App({Component, pageProps}: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return <>
+        <Head>
+            <title>My best app</title>
+            <link key={1} rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Component {...pageProps} />
+    </>;
 }
 
 export default App;
